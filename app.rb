1886499268bebae1e19e2b706311c1aa76046932
JosueBravo1998 <josue.bravo.simontech.2016@gmail.com>
2016-03-04 17:48:14 +0000

    done kind of

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
   post '/' do
     @user = {
       name: "#{params["fname"]} #{params["lname"]}",
+     
       profile_pic_url: "#{params["picurl"]}"
       }
        erb :dashboard
