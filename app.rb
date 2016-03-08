af71708670abbdcfc360b74d01616327b3c64151
josuebravo1998 <josue.bravo.simontech.2016@gmail.com>
2016-03-08 23:19:32 +0000

    third commit

--- app.rb
+++ app.rb
@@ -11,8 +11,11 @@ class App < Sinatra::Base
     @user = {
       name: "#{params["fname"]} #{params["lname"]}",
      
-      profile_pic_url: "#{params["picurl"]}"
-      }
+      profile_pic_url: "#{params["picurl"]}",
+      current_date: "#{params["date"]}",
+      tweet: "#{params["tweet"]}",
+     month_and_date: "#{params["month&date"]}"
+    }
        erb :dashboard
    end
  
